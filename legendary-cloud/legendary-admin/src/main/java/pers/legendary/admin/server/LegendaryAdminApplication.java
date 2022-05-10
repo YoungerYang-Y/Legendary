9b9eec928bed7d9760fcd13a0f24041284f4bfdd
yangyang <1059376437@qq.com>
2022-05-10 23:40:49 +0800

    通过Nacos向SpringbootAdmin的Server中注册Client。【注意：Server需要做最后启动，才能获取到所以都Client】

--- legendary-cloud/legendary-admin/src/main/java/pers/legendary/admin/server/LegendaryAdminApplication.java
+++ legendary-cloud/legendary-admin/src/main/java/pers/legendary/admin/server/LegendaryAdminApplication.java
@@ -3,6 +3,7 @@ package pers.legendary.admin.server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Description: Spring Boot Admin
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/4/26 14:33
  */
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LegendaryAdminApplication {
     public static void main(String[] args) {
