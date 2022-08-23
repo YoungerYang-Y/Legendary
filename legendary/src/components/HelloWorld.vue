6a7e6616432d8bd05f0dc1192fbb5888f74dbe95
yangyang <1059376437@qq.com>
2022-08-23 15:17:09 +0000

    [Module] legendary [Title] 格式化 [Desc] 解决一些语法错误

--- legendary/src/components/HelloWorld.vue
+++ legendary/src/components/HelloWorld.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
-}>()
+  msg: string;
+}>();
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      You`ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
       <a target="_blank" href="https://vuejs.org/">Vue 3</a>. What's next?
     </h3>
