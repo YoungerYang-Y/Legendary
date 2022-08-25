666b0593ffb6301a76a479e42b811e420ba6f64d
yangyang <1059376437@qq.com>
2022-08-25 17:25:15 +0000

    完整引入ant-design-vue

--- legendary/src/main.ts
+++ legendary/src/main.ts
@@ -4,8 +4,12 @@ import router from "./router";
 
 import "./assets/main.css";
 
+// 完整引入ant-design-vue
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
 const app = createApp(App);
 
-app.use(router);
+app.use(router).use(Antd);
 
 app.mount("#app");
