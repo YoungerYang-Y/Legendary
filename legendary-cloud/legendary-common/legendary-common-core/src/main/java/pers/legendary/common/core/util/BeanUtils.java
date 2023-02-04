4504cf4d0a6954102a1c77ebf395d117540fa872
YangYang <1059376437@qq.com>
2023-02-04 11:25:42 +0800

    [Module] common-core [Title] BeanUtils [Desc] 删除callback，增加可忽略自动

--- legendary-cloud/legendary-common/legendary-common-core/src/main/java/pers/legendary/common/core/util/BeanUtils.java
+++ legendary-cloud/legendary-common/legendary-common-core/src/main/java/pers/legendary/common/core/util/BeanUtils.java
@@ -18,38 +18,20 @@ public class BeanUtils extends BeanUtil {
     /**
      * 深拷贝List
      *
+     * @param sources          被拷贝对象
+     * @param target           目标类::new(eg: UserVO::new)
+     * @param ignoreProperties 不拷贝的的属性列表
+     * @return 深拷贝结果
      * @author YangYang
      * @date 2023-02-03 22:49
-     * @param sources 被拷贝对象
-     * @param target 目标类::new(eg: UserVO::new)
-     * @return 深拷贝结果
      */
-    public static <S, T> List<T> copyListProperties(List<S> sources, Supplier<T> target) {
-        return copyListProperties(sources, target, null);
-    }
-
-    /**
-     * 深拷贝List
-     *
-     * @author YangYang
-     * @date 2023-02-03 22:49
-     * @param sources 被拷贝对象
-     * @param target 目标类::new(eg: UserVO::new)
-     * @param callBack 回调函数
-     * @return 深拷贝结果
-     */
-    public static <S, T> List<T> copyListProperties(List<S> sources, Supplier<T> target, BeanCopyUtilCallBack<S, T> callBack) {
+    public static <S, T> List<T> copyListProperties(List<S> sources, Supplier<T> target, String... ignoreProperties) {
         List<T> list = new ArrayList<>(sources.size());
         for (S source : sources) {
             T t = target.get();
-            copyProperties(source, t);
+            copyProperties(source, t, ignoreProperties);
             list.add(t);
-            if (callBack != null) {
-                // 回调
-                callBack.callBack(source, t);
-            }
         }
         return list;
     }
-
 }
